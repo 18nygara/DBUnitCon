1fa6967f19e9beef109b607edcdabe33a7a410bb
Unknown <dragynmada@gmail.com>
2018-02-28 18:53:58 -0600

    Everything works, slight touch up probably needed

---
+++ DatabaseActivities/Controllers/MileFathomController.cs
@@ -0,0 +1,55 @@
+﻿using DatabaseActivities.Models.Entity;
+using DatabaseActivities.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCUnitConV1.Controllers
+{
+    public class MileFathomController : Controller
+    {
+        private MileFathomService service;
+
+        public MileFathomController()
+        {
+            service = new MileFathomService();
+        }
+
+        // GET: MileFathom
+        public ActionResult Index()
+        {
+            return View(service.GetAllMileFathoms());
+        }
+
+        [HttpPost]
+        public ActionResult MileToFathom(double mile)
+        {
+            double fathom = mile * 880;
+            MileFathom test = new MileFathom();
+            test.Mile = mile;
+            test.Fathom = fathom;
+            service.AddMileFathom(test);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult FathomtoMile(double fathom)
+        {
+            double mile = fathom / 880;
+            MileFathom test = new MileFathom();
+            test.Mile = mile;
+            test.Fathom = fathom;
+            service.AddMileFathom(test);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            MileFathom toDelete = service.GetMileFathomById(id);
+            service.RemoveMileFathom(toDelete);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DatabaseActivities/Repositories/MileFathomRepository/MileFathomRepository.cs
@@ -0,0 +1,41 @@
+﻿using DatabaseActivities.Models;
+using DatabaseActivities.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseActivities.Repositories.MileFathomRepository
+{
+    public class MileFathomRepository
+    {
+        private ApplicationDbContext context;
+
+        public MileFathomRepository()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        public List<MileFathom> GetAllMileFathoms()
+        {
+            return context.MileFathoms.ToList();
+        }
+
+        public void AddMileFathom(MileFathom toAdd)
+        {
+            context.MileFathoms.Add(toAdd);
+            context.SaveChanges();
+        }
+
+        public void RemoveMileFathom(MileFathom toDelete)
+        {
+            context.MileFathoms.Remove(toDelete);
+            context.SaveChanges();
+        }
+
+        public MileFathom GetMileFathomById(int id)
+        {
+            return context.MileFathoms.Find(id);
+        }
+    }
+}
